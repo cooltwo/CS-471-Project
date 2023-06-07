d03ff0007903eafa1279a60943cb856a5d1f9f41
group <ethan_neu@yahoo.com>
2023-06-07 00:29:18 -0400

    Add additional API functions, including user creation

--- server.js
+++ server.js
@@ -99,6 +99,99 @@ app.post('/api/resumeUpload', (req,res) => {
     });
 });
 
+AVAILABLE_INIT_ARRAY = [[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]];
+
+app.post('/api/newstudent', (req,res) => {
+   user_path = "database/logins/" + req.body.username + ".json";
+   info_path = "database/userinfo/" + req.body.username + ".json";
+   if (!fs.existsSync(user_path))
+   {
+       data = {username:req.body.username, password:req.body.password, userType:0};
+       firstWriteSuccess = false;
+       fs.writeFile(user_path, data, err => {
+           if(err)
+           { res.json({response: "cannot create file"}) }
+           else
+           { firstWriteSuccess = true }
+       });
+       if (firstWriteSuccess) {
+           data = {username:req.body.username, usertype:0, courses:[], userfiles:[], sessions:[], available:AVAILABLE_INIT_ARRAY};
+           fs.writeFile(info_path, data, err => {
+               if(err)
+               { res.json({response: "cannot create file"}) }
+               else
+               { res.json({response: "approved", username:req.body.username, userType:0}) }
+           });
+       }
+   }
+   else
+   { res.json({response: "already exists"}) }
+});
+
+app.post('/api/newtutor', (req,res) => {
+   user_path = "database/logins/" + req.body.username + ".json";
+   info_path = "database/userinfo/" + req.body.username + ".json";
+   if (!fs.existsSync(user_path))
+   {
+       data = {username:req.body.username, password:req.body.password, userType:1};
+       firstWriteSuccess = false;
+       fs.writeFile(user_path, data, err => {
+           if(err)
+           { res.json({response: "cannot create file"}) }
+           else
+           { firstWriteSuccess = true }
+       });
+       if (firstWriteSuccess) {
+           data = {username:req.body.username, usertype:1, courses:[], userfiles:[], sessions:[], available:AVAILABLE_INIT_ARRAY};
+           fs.writeFile(info_path, data, err => {
+               if(err)
+               { res.json({response: "cannot create file"}) }
+               else
+               { res.json({response: "approved", username:req.body.username, userType:1}) }
+           });
+       }
+   }
+   else
+   { res.json({response: "already exists"}) }
+});
+
+app.post('/api/tutorsForCourse', (req,res) => {
+    course_path = "database/courses/" + req.body.course + ".json"
+    if (fs.existsSync(course_path)){
+        tutors_list = [];
+        fs.readdirSync('database/userinfo/').forEach(file => {
+            fs.readFile(file, (err, data) => {
+                if(err)
+                { res.json({response: "this function was probably made incorrectly"}) }
+                else {
+                    dataJSON = JSON.parse(data);
+                    if (dataJSON.usertype == 1 && dataJSON.courses.includes(req.body.course)) {
+                        tutors_list.push(dataJSON.username)
+                    }
+                }
+            });
+        });
+        if (tutors_list.length == 0)
+        { res.json({response: "no tutors found for course"}) }
+        else
+        { res.json({response: "success", tutors:tutors_list}) }
+    }
+    else
+    { res.json({response: "course not found"}) }
+});
+
+app.post('/api/myCourses', (req,res) => {
+    user_path = "database/userinfo/" + req.body.username + ".json"
+    fs.readFile(user_path, (err, data) => {
+        if(err)
+        { res.json({response: "user not found"}) }
+        else {
+            dataJSON = JSON.parse(data);
+            res.json({response:"success", courses:dataJSON.courses.stringify()})
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
