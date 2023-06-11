10a40707ca5354265efd2c8b1d379a9bcfc6bc50
stell2431 <stell2431@gmail.com>
2023-06-11 17:30:38 -0400

    cleaned up video call UI, added to the project and integrated with site

---
+++ angular-app/src/app/video call/video-session.js
@@ -0,0 +1,130 @@
+// create Agora client
+var client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+
+var localTracks = {
+  videoTrack: null,
+  audioTrack: null
+};
+var remoteUsers = {};
+// Agora client options
+var options = {
+  appid: null,
+  channel: null,
+  uid: null,
+  token: null
+};
+
+const APP_ID = "a6ad7658ffe9426ba9e88950390b5970"; //the app ID from agora for this project
+
+//upon clicking the join button
+$("#join-form").submit(async function (e) {
+  e.preventDefault();
+  $("#join").attr("disabled", true); //you can't join twice
+
+  try {
+    options.appid = APP_ID; //get the constant app ID set earlier
+    options.channel = "main"; //generateChannel(); //generate the channel to be used
+
+    await join(); //run the join function to join the session
+
+    $("#success-alert a").attr("href", `video-session.html?appid=${options.appid}&channel=${options.channel}`);
+    $("#success-alert").css("display", "block");
+    
+  } catch (error) {
+    console.error(error);
+  } finally {
+    $("#leave").attr("disabled", false);
+  }
+})
+
+//on clicking the leave button
+$("#leave").click(function (e) {
+  leave();
+})
+
+//create a custom channel based on session time
+async function generateChannel() {
+    //let numID = homePage.sessionDayOfWeek; TODO
+    return '../home-page/home-page.component'.sessionDayOfWeek;
+}
+
+//join the session
+async function join() {
+
+  //add event listener to play remote tracks when remote user publishs.
+  client.on("user-published", handleUserPublished);
+  client.on("user-unpublished", handleUserUnpublished);
+
+  //join a channel and create local tracks, we can use Promise.all to run them concurrently
+  [ options.uid, localTracks.audioTrack, localTracks.videoTrack ] = await Promise.all([
+    client.join(options.appid, options.channel, options.token || null), //join the channel
+    AgoraRTC.createMicrophoneAudioTrack(), //create local audio tracks with user microphone
+    AgoraRTC.createCameraVideoTrack() //create local video track with user camera
+  ]);
+  
+  //play local video track
+  localTracks.videoTrack.play("local-player");
+  $("#local-player-name").text(`localVideo(${options.uid})`);
+
+  //publish local tracks to channel
+  await client.publish(Object.values(localTracks));
+  console.log("publish success");
+}
+
+//leave the session
+async function leave() {
+  for (trackName in localTracks) {
+    var track = localTracks[trackName];
+    if(track) {
+      track.stop();
+      track.close();
+      localTracks[trackName] = undefined;
+    }
+  }
+
+  //remove remote users and player views
+  remoteUsers = {};
+  $("#remote-playerlist").html("");
+
+  await client.leave(); //leave the channel
+
+  $("#local-player-name").text("");
+  $("#join").attr("disabled", false);
+  $("#leave").attr("disabled", true);
+  console.log("client leaves channel success");
+}
+
+//subscribe to a remote user
+async function subscribe(user, mediaType) {
+  const uid = user.uid;
+
+  await client.subscribe(user, mediaType); 
+
+  console.log("subscribe success");
+
+  if (mediaType === 'video') {
+    const player = $(`
+      <div id="player-wrapper-${uid}">
+        <div id="player-${uid}" class="player"></div>
+      </div>
+    `);
+    $("#remote-playerlist").append(player);
+    user.videoTrack.play(`player-${uid}`);
+  }
+
+  if (mediaType === 'audio') {
+    user.audioTrack.play();
+  }
+}
+
+function handleUserPublished(user, mediaType) {
+  const id = user.uid;
+  remoteUsers[id] = user;
+  subscribe(user, mediaType);
+}
+
+function handleUserUnpublished(user) {
+  const id = user.uid;
+  delete remoteUsers[id];
+  $(`#player-wrapper-${id}`).remove();
+}
